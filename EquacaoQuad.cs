2eb2e6ccf9c26076fb2b59dae28cbe6bb24b2307
tuyanaXu <tuyana.xu@gmail.com>
2024-09-01 23:48:12 -0300

    Add files via upload
    Bhaskara

---
+++ EquacaoQuad.cs
@@ -0,0 +1,64 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+// Projeto:
+namespace EquacaoQuadCSharp
+{
+    //Classe:
+    class EquacaoQuad
+    {
+        // Atributo privado:
+        private double _delta;
+
+        // Propriedades:
+        public int A { get; set; }
+        public int B { get; set; }
+        public int C { get; set; }
+
+        public double? X1 { get; private set; }
+        public double? X2 { get; private set; }
+        public byte Solucao { get; private set; }
+
+        // Δ = b² – 4.a.c
+        private void CalcDiscriminante() 
+        {
+            _delta = Math.Pow(B, 2) - 4 * A * C;
+
+            // A equação não terá raízes reais;
+            if (_delta < 0)
+                Solucao = 0;
+
+            // A equação apresentará duas raízes reais e iguais;
+            else if (_delta == 0)
+                Solucao = 1;
+
+            // senao Δ > 0: A equação vai ter duas raízes reais e distintas.
+            else
+                Solucao = 2;
+        }
+
+        // x = - b ± √Δ / 2.a
+        private void CalcX1eX2() 
+        {
+            var raizQuadDelta = Math.Sqrt(_delta);
+
+            X1 = (-B + raizQuadDelta) / (2 * A);
+            X2 = (-B - raizQuadDelta) / (2 * A);
+        }
+        public void Bhaskara() 
+        {
+            CalcDiscriminante();
+
+            if (Solucao != 0)
+                CalcX1eX2();
+            else
+            {
+                X1 = null;
+                X2 = null;
+            }
+        }
+    }
+}
---
+++ FrmPrincipal.cs
@@ -0,0 +1,53 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System
+    .Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace EquacaoQuadCSharp
+{
+    public partial class FrmPrincipal : Form
+    {
+        public FrmPrincipal()
+        {
+            InitializeComponent();
+        }
+
+        private void btnCalcular_Click(object sender, EventArgs e)
+        {
+            EquacaoQuad eq = new EquacaoQuad();
+            eq.A = Convert.ToInt32(txtA.Text);
+            eq.B = Convert.ToInt32(txtB.Text);
+            eq.C = Convert.ToInt32(txtC.Text);
+            eq.Bhaskara();
+            Limpar();
+            
+            if(eq.A == 0)
+                MessageBox.Show("O coeficiente a não pode ser zero.");
+            
+            else if (eq.Solucao == 0)
+                MessageBox.Show("A equação não possui raízes reais");
+            
+            else if (eq.Solucao == 1)
+                MessageBox.Show("x' e x\" = " + eq.X1.ToString());
+            
+            else
+                MessageBox.Show("x' = " + eq.X1.ToString() + "\nx\" = " + eq.X2.ToString());
+        }
+        private void Limpar() 
+        {
+            txtA.Clear();
+            txtB.Clear();
+            txtC.Clear();
+            txtA.Focus();
+
+        }
+
+        
+    }
+}
